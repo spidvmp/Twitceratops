a1b5a48f8c5cdf09760c15f6aec67c06a2f27565
Vicente de Miguel <spidvmp@gmail.com>
2016-05-02 21:46:53 +0200

    creado modelo lista de tweets

---
+++ app/src/main/java/com/nicatec/twitceratops/model/Tweets.java
@@ -0,0 +1,40 @@
+package com.nicatec.twitceratops.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by vtx on 2/5/16.
+ */
+public class Tweets {
+    List<TweetMessage> tweets;
+
+    public static Tweets createTweets(List<TweetMessage> tweets) {
+        Tweets myTweets = new Tweets();
+
+        for (TweetMessage t: tweets) {
+            myTweets.add(t);
+        }
+
+        return myTweets;
+    }
+
+    private Tweets() { }
+
+    public int size() { return getTweets().size(); }
+
+    public TweetMessage get(int index) { return getTweets().get(index);}
+
+    public void remove(TweetMessage t) { getTweets().remove(t); }
+
+    public void clear() { getTweets().clear(); }
+
+    public void add(TweetMessage t) {getTweets().add(t); }
+
+    public List<TweetMessage> getTweets() {
+        if (this.tweets == null) {
+            this.tweets = new LinkedList<>();
+        }
+        return this.tweets;
+    }
+}
