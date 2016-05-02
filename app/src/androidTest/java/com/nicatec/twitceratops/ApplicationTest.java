771f126d262128a715eba168cd58df432a1e0fea
Vicente de Miguel <spidvmp@gmail.com>
2016-05-02 22:02:00 +0200

    test de la lista de tweets

--- app/src/androidTest/java/com/nicatec/twitceratops/ApplicationTest.java
+++ app/src/androidTest/java/com/nicatec/twitceratops/ApplicationTest.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 import com.nicatec.twitceratops.model.TweetMessage;
+import com.nicatec.twitceratops.model.Tweets;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -13,11 +17,13 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
+    final String mens = "Hola";
+    final String photo = "url";
+    final float lat = 5.9839453f;
+    final float lon = -45.2346234f;
+
     public void test_Creo_Un_Mensaje() {
-        final String mens = "Hola";
-        final String photo = "url";
-        final float lat = 5.9839453f;
-        final float lon = -45.2346234f;
+
         final TweetMessage m = new TweetMessage(0,mens, photo, lat, lon);
         assertEquals(m.getMessage() , mens);
         assertEquals(m.getPhotoUrl(), photo);
@@ -26,15 +32,29 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     }
 
     public void test_Creo_Mensaje_Con_Valores_Nulos(){
-        final String mens = "";
-        final String photo = "";
-        final float lat = 5.9839453f;
-        final float lon = -45.2346234f;
         final TweetMessage m = new TweetMessage(0,null, null, lat, lon);
-        assertEquals(m.getMessage() , mens);
-        assertEquals(m.getPhotoUrl(), photo);
+        assertEquals(m.getMessage() , "");
+        assertEquals(m.getPhotoUrl(), "");
         assertEquals(m.getLatitude(), lat);
         assertEquals(m.getLongitude(), lon);
+
+    }
+
+    public void test_Creo_Mensajes_En_La_Lista_De_Tweets_Y_Los_Borro() {
+        final TweetMessage m1 = new TweetMessage(0,mens,photo,lat,lon);
+        final TweetMessage m2 = new TweetMessage(0,"sss","photo",lat,lon);
+        List<TweetMessage> tm = new LinkedList<>();
+
+        Tweets tw = Tweets.createTweets(tm);
+        tw.add(m1);
+        tw.add(m2);
+        assertEquals(tw.size(),2);
+
+        tw.remove(m1);
+        assertEquals(tw.size(),1);
+
+        tw.clear();
+        assertEquals(tw.size(),0);
 
     }
 }
